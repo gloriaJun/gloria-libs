839dfa861ad9525b567e45a3da991291716d9484
gloriaJun <pureainu@gmail.com>
2020-09-23 20:52:49 +0900

    chore: add commitlint

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': tasks(['lint-staged']),
     // 'pre-push': tasks([
     //   'npm run lint',
