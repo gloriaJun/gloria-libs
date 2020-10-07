2512e79768b26c28f51614cb5ad380560c90f4e0
gloriaJun <pureainu@gmail.com>
2020-10-07 12:30:56 +0900

    feat: toast popup

---
+++ packages/react/ui/src/pop-up/toast/style.tsx
@@ -0,0 +1,9 @@
+import tw from 'twin.macro';
+
+const animation = 'transition-transform duration-1000 ease-in-out';
+
+export const ToastContainer = tw.div`fixed z-50 w-4/5 bottom-0 mb-3`;
+
+export const ToastContent = tw.div`relative w-full bg-green-300 text-gray-800 rounded-sm items-center mt-2 py-1 px-4`;
+
+export const ToastMessage = tw.p`leading-tight`;
