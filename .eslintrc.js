8f165295fa465e4dcf15d5c69422f568dc28056d
gloria <pureainu@gmail.com>
2021-01-19 00:01:58 +0900

    chore: modify eslint to apply custom rule plugin (#15)

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,90 +18,17 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks', 'jest'],
+  plugins: ['jest'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    'gloria-base',
+    'plugin:react/recommended',
     'plugin:jest/recommended',
+    'prettier/react',
+    'plugin:jest/recommended',  
   ],
   settings: {},
-  rules: {
-    // defined the common lint rules
-    /**
-     * basic rules
-     */
-    // https://eslint.org/docs/rules/no-unused-vars
-    '@typescript-eslint/no-unused-vars': ['error'],
-
-    /**
-     * typescript
-     */
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-use-before-define': 'warn',
-
-    /**
-     * import
-     */
-    'import/order': [
-      'error',
-      {
-        groups: [
-          ['builtin', 'external'],
-          'internal',
-          'parent',
-          ['sibling', 'index'],
-        ],
-        'newlines-between': 'always',
-        alphabetize: {
-          caseInsensitive: true,
-          order: 'asc',
-        },
-      },
-    ],
-    'import/exports-last': 'error',
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        js: 'never',
-        mjs: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-        vue: 'never',
-      },
-    ],
-    'import/first': 'error',
-    'import/group-exports': 'off',
-    'import/newline-after-import': 'error',
-    'import/no-extraneous-dependencies': ['error'],
-    'import/no-self-import': 'error',
-    'import/no-cycle': 'error',
-    'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
-    'import/prefer-default-export': 'off',
-    'import/no-absolute-path': 'error',
-    'import/no-internal-modules': 'error',
-    'import/no-mutable-exports': 'error',
-    'import/no-named-as-default-member': 'error',
-    'import/no-named-as-default': 'error',
-    'import/no-named-default': 'error',
-    'import/no-named-export': 'off',
-    'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
-    'import/no-webpack-loader-syntax': 'error',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        // devDependencies: ["*.js", "src/**/*.test.js"],
-        devDependencies: true,
-        optionalDependencies: true,
-        peerDependencies: true,
-        bundledDependencies: true,
-      },
-    ],
-  },
+  rules: {},
+  ignorePatterns: ['node_modules', '*.config.js', '.*rc.js', 'dist'],
   overrides: [
     {
       files: ['./packages/react/**/*.tsx'],
