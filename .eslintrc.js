c5ac2cfa9d58158d6e965630a5b6f2401da0bf92
gloriaJun <pureainu@gmail.com>
2021-01-18 23:35:11 +0900

    chore: modify eslint to apply custom rule plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
@@ -13,17 +19,12 @@ module.exports = {
     warnOnUnsupportedTypeScriptVersion: false,
     // ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features,
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['jest'],
   extends: [
-    'eslint:recommended',
+    'gloria-base',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/typescript',
     'plugin:jest/recommended',
     'prettier/react',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   settings: {
@@ -31,39 +32,8 @@ module.exports = {
       version: 'detect',
     },
   },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-  },
-  // ignorePatterns: ['node_modules', '*.config.js', 'build', 'dist'],
-  rules: {
-    // defined the common lint rules
-    /**
-     * basic rules
-     */
-    // https://eslint.org/docs/rules/no-unused-vars
-    '@typescript-eslint/no-unused-vars': ['error'],
-
-    /**
-     * typescript
-     */
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-use-before-define': 'warn',
-
-    /**
-     * react
-     */
-
-    /**
-     * import
-     */
-    // 'import/no-extraneous-dependencies': ['error'],
-    // 'import/no-self-import': 'error',
-    // 'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
-    // 'import/prefer-default-export': 'off'
-  },
+  rules: {},
+  ignorePatterns: ['node_modules', '*.config.js', '.*rc.js', 'dist'],
   overrides: [
     // all test files
     // {
