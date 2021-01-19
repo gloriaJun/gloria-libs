08b9576538c9d326ab77470e7f05993fb3d4eeef
gloria <pureainu@gmail.com>
2021-01-20 01:08:43 +0900

    chore: storybook version update (#18)

--- packages/react/storybook/.storybook/main.js
+++ packages/react/storybook/.storybook/main.js
@@ -1,39 +1,20 @@
+const path = require('path');
+
+const resolve = (dir) => path.resolve(__dirname, `../../${dir}`);
+
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../../**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-    // '@storybook/addon-actions',
-  ],
+  stories: [resolve(`ui/**/*.stories.@(js|jsx|ts|tsx|mdx)`)],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
-            plugins: [
-              require.resolve('@babel/plugin-proposal-class-properties'),
-
-              // To generate props
-              require.resolve('babel-plugin-react-docgen'),
-            ],
-          },
-        },
-      ],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    /*
-     * custom configuration
-     */
-
     return config;
   },
 };
---
+++ packages/utility/logger/src/index.ts
@@ -0,0 +1,3 @@
+export default function logger(message: string): void {
+  console.log(message);
+}
