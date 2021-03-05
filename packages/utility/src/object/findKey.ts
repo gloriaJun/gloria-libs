0e33c5513abc5f249303cddd9853e9cdee8af3b0
gloriaJun <pureainu@gmail.com>
2021-03-05 13:59:07 +0900

    feat: add findKey utility

---
+++ packages/utility/src/object/findKey.ts
@@ -0,0 +1,20 @@
+type IObjectType<T> = GeneralObject<T> | null;
+
+export const findKey = <T>(obj: IObjectType<T>, value?: T) => {
+  let result = null;
+
+  if (obj) {
+    const keys = Object.keys(obj);
+    const len = keys.length;
+
+    for (let i = 0; i < len; i++) {
+      const key = keys[i];
+      if (obj[key] === value) {
+        result = key;
+        break;
+      }
+    }
+  }
+
+  return result;
+};
--- packages/utility/src/object/hasKeys.ts
+++ packages/utility/src/object/hasKeys.ts
@@ -1,31 +1,36 @@
 import { isObject } from '../validator';
 
+type IObjectType<T> = GeneralObject<T> | Array<T> | null;
+type IKeyType = undefined | string;
+type IKeysType = IKeyType | string[];
+
 const methods = {
   every: 'every',
   some: 'some',
 } as const;
 
-const hasKeys = (
+const hasKeys = <T>(
   method: typeof methods[keyof typeof methods],
-  obj: unknown,
-  key: string | string[],
+  obj: IObjectType<T>,
+  key: IKeysType,
 ) => {
   return (
     isObject(obj) &&
+    key &&
     (typeof key === 'string' ? [key] : key)[method]((k) => {
       return Object.prototype.hasOwnProperty.call(obj, k);
     })
   );
 };
 
-export const hasEveryKeys = (obj: unknown, key: string | string[]) => {
+export const hasEveryKeys = <T>(obj: IObjectType<T>, key: IKeysType) => {
   return hasKeys(methods.every, obj, key);
 };
 
-export const hasSomeKeys = (obj: unknown, key: string | string[]) => {
+export const hasSomeKeys = <T>(obj: IObjectType<T>, key: IKeysType) => {
   return hasKeys(methods.some, obj, key);
 };
 
-export const hasKey = (obj: unknown, key: string) => {
+export const hasKey = <T>(obj: IObjectType<T>, key: IKeyType) => {
   return hasKeys(methods.some, obj, key);
 };
