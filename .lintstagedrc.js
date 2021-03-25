ca713b087cd9d54b8d8c510e163d36cd6a28c5fe
gloriaJun <pureainu@gmail.com>
2021-03-25 17:31:45 +0900

    chore: modify husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,json,md}': ['prettier --write', 'git add'],
-  '*.{js,jsx,ts,tsx}': ['yarn lint:fix', 'prettier --write', 'git add'],
-  // '*.{js,jsx,ts,tsx}': ['npm run lint:fix', 'npm run prettier:fix', 'git add'],
-  //   '{*.{json,md}}': ['prettier --write', 'git add'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
+  '*.{ts,tsx}': [
+    () => 'tsc --skipLibCheck --noEmit',
+    'eslint --fix',
+    'prettier --write',
+    'git add',
+  ],
+  '{*.{json,md}}': ['prettier --write', 'git add'],
 };
