f56cfa5da3b5e3c5e486c1124d53d8cff0d73b34
gloria <pureainu@gmail.com>
2021-03-27 17:01:21 +0900

    feat: add usePrevious hook (#29)
    * chore: modify package.json
    
    * feat: add usePrevious hook
    
    * chore: modify package file
    
    * fix: modify story

--- packages/react/hooks/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div className="App">AAA</div>;
-}
-
-export default App;
--- packages/react/storybook/.storybook/main.js
+++ packages/react/storybook/.storybook/main.js
@@ -4,7 +4,10 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const resolve = (dir) => path.resolve(process.cwd(), `..`, dir);
 
 module.exports = {
-  stories: [resolve(`ui/**/*.stories.@(js|jsx|ts|tsx|mdx)`)],
+  stories: [
+    resolve(`ui/**/*.stories.@(js|jsx|ts|tsx|mdx)`),
+    resolve(`hooks/**/*.stories.@(js|jsx|ts|tsx|mdx)`),
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
