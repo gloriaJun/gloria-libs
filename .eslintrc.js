23d24f50371d5fc52fdec782a3439a84d8f5b248
gloriaJun <pureainu@gmail.com>
2021-03-27 16:50:44 +0900

    chore: modify package file

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,42 +68,39 @@ module.exports = {
     {
       files: ['./packages/react/**/*.stories.tsx'],
       rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
-          { packageDir: [`./`, `./packages/react/ui`] },
-        ],
-        'import/no-internal-modules': [
-          'error',
-          {
-            allow: ['@storybook/**'],
-          },
-        ],
-        'import/exports-last': 'off',
-        'import/order': [
-          'error',
-          {
-            groups: [
-              'builtin',
-              'external',
-              'internal',
-              'parent',
-              ['sibling', 'index'],
-            ],
-            pathGroups: [
-              {
-                pattern: '@storybook/**',
-                group: 'external',
-                position: 'after',
-              },
-            ],
-            pathGroupsExcludedImportTypes: ['@storybook/**'],
-            'newlines-between': 'always',
-            alphabetize: {
-              caseInsensitive: true,
-              order: 'asc',
-            },
-          },
-        ],
+        'import/no-extraneous-dependencies': 'off',
+        //   'import/no-internal-modules': [
+        //     'error',
+        //     {
+        //       allow: ['@storybook/**'],
+        //     },
+        //   ],
+        //   'import/exports-last': 'off',
+        //   'import/order': [
+        //     'error',
+        //     {
+        //       groups: [
+        //         'builtin',
+        //         'external',
+        //         'internal',
+        //         'parent',
+        //         ['sibling', 'index'],
+        //       ],
+        //       pathGroups: [
+        //         {
+        //           pattern: '@storybook/**',
+        //           group: 'external',
+        //           position: 'after',
+        //         },
+        //       ],
+        //       pathGroupsExcludedImportTypes: ['@storybook/**'],
+        //       'newlines-between': 'always',
+        //       alphabetize: {
+        //         caseInsensitive: true,
+        //         order: 'asc',
+        //       },
+        //     },
+        //   ],
       },
     },
   ],
